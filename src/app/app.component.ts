8afe064c8cf473d5e2020946c17afe7e21672720
Cashapona <rajani.bathula@cashapona.com>
2019-08-13 15:49:05 +0530

    header and login design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'self-service-portal';
+
+  noHeader: any = true;
+
+  constructor(private router: Router) {
+    // hide header on login page
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url.indexOf('/home') !== -1 || (event.urlAfterRedirects && event.urlAfterRedirects.indexOf('/home') !== -1)) {
+          this.noHeader = true;
+        } else {
+          this.noHeader = false;
+        }
+      }
+    });
+  }
 }
